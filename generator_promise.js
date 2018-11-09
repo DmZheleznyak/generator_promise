d46ab395485cd2fbb5b236a34de74ba40ae88f3e
Dmitriy <11lione1991@gmail.com>
2018-11-09 15:45:39 +0200

    delete typeError

--- generator_promise.js
+++ generator_promise.js
@@ -15,15 +15,18 @@ function run(gen) {
 	var generatorForPromise = gen()   
 
 	function executor( generator, yieldValue ) {
-		
+
 		var next = generator.next( yieldValue )
+		if ( !next.done ) {
+			
+			if ( typeof next.value === 'function'  ) return executor( generator, next.value() )
 
-		if ( typeof next.value === 'function'  ) return executor( generator, next.value() )
+			if ( next.value.toString() === "[object Promise]" ) return next.value.then( result => executor( generator, result ) )
 
-		if ( next.value.toString() === "[object Promise]" ) return next.value.then( result => executor( generator, result ) )
+			if ( typeof next.value === 'number') return executor( generator, next.value )
 
-		if ( typeof next.value === 'number') return executor( generator, next.value )
-
+		}
+		
 	}  
 
 	executor( generatorForPromise )  
